081fa70f90b126e1a452769852d54e250a3793ad
Tanvir Rahman <rahmantanvir25558@gmail.com>
2020-11-22 16:30:52 +0600

    build

---
+++ dist/js/notFound.4aca5ecc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["notFound"],{"5eec":function(n,e,t){"use strict";t("e03d")},"8cdb":function(n,e,t){"use strict";t.r(e);var c=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"not-found"},[n._v(" Page not found ")])},o=[],u=(t("5eec"),t("2877")),s={},i=Object(u["a"])(s,c,o,!1,null,"495b9308",null);e["default"]=i.exports},e03d:function(n,e,t){}}]);
+//# sourceMappingURL=notFound.4aca5ecc.js.map
